66ae1d5e6cebc21b9a0436a2f0988bd8f48f24db
Zakaria <69237503+ariadiki@users.noreply.github.com>
2021-11-25 10:03:38 +0100

    Update forget.php

--- forget.php
+++ forget.php
@@ -16,8 +16,8 @@
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'kizoing@gmail.com';                     //SMTP username
-    $mail->Password   = 'Ziko2021';                               //SMTP password
+    $mail->Username   = 'email@email.email';                     //SMTP username
+    $mail->Password   = 'password';                               //SMTP password
     $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;     
     $mail->isHTML(true);
